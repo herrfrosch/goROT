485f24101c935e41fa9fbae284d460163d0b6134
Krzysztof Zabski <froschkris@gmail.com>
2022-01-21 19:33:06 +0100

    Added adjustable ROT shift

--- goROT.go
+++ goROT.go
@@ -6,23 +6,26 @@ import(
 	"os"
 )
 
-func encrypt(text string)string{
+func encrypt(text string, shift int)string{
+
+	if shift>26 {
+		shift = shift-26
+	}
 
 	letters := [26]string{"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"}
 
 	var lengthText = len([]rune(text))
-	//var lengthLet = len([]rune(letters))   
 
 	var encrypted string
 
 	for i := 0; i < lengthText; i++{
 		for a := 0; a < 26; a++{
 			if string(text[i]) == letters[a] {
-				if a+13 > 25 {
-					index := (a+13)-26
+				if a+shift > 25 {
+					index := (a+shift)-26
 					encrypted = encrypted+letters[index]
 				} else {
-					encrypted = encrypted+letters[a+13]
+					encrypted = encrypted+letters[a+shift]
 				}
 			}
 		}
@@ -33,7 +36,13 @@ func encrypt(text string)string{
 
 func main(){
 
-	fmt.Println("Enter word you want to encrypt: ")
+	fmt.Println("How many letters you want to shift (eg. ROT13 - input is shifted by 13 letters): ")
+
+	var shift int
+
+	fmt.Scanln(&shift)
+
+	fmt.Println("Enter words you want to encrypt: ")
 
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
@@ -42,7 +51,7 @@ func main(){
 
 	var encrypted string
 
-	encrypted = encrypt(toEncrypt)
+	encrypted = encrypt(toEncrypt, shift)
 
 	fmt.Println("input: ", toEncrypt, " output: ", encrypted)
 }
